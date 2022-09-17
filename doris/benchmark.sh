ee3dc410576b5bb6a6cd27bf3a8cb4686b03570e
stephen <hello_stephen@qq.com>
2022-09-17 16:05:41 +0800

    update 1

--- doris/benchmark.sh
+++ doris/benchmark.sh
@@ -4,18 +4,20 @@
 
 # Install
 url='https://doris-build-1308700295.cos.ap-beijing.myqcloud.com/tmp/master-942b31038-release-20220917020007.tar.gz'
-# wget --continue ${url}
-DORIS_HOME="$(basename ${url} | cut -d'.' -f1)"
-mkdir "$DORIS_HOME"
-tar zxvf "$(basename ${url})" -C "$DORIS_HOME"
-cd "$DORIS_HOME/output" || exit
+wget --continue ${url}
+dir_name="$(basename ${url} | cut -d'.' -f1)"
+if [[ -d $dir_name ]]; then rm -rf "$dir_name"; else mkdir "$dir_name"; fi
+tar zxvf "$(basename ${url})" -C "$dir_name"
+cd "$dir_name/output" || exit
 DORIS_HOME=$(pwd)
 export DORIS_HOME
 
 # Install dependencies
-sudo yum install -y java-1.8.0-openjdk-devel.x86_64 mysql
-
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk/
+# sudo yum install -y java-1.8.0-openjdk-devel.x86_64 mysql
+# export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk/
+# export PATH=$JAVA_HOME/bin:$PATH
+sudo yum install -y mysql java-11-openjdk.x86_64
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk/"
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Create directory for FE and BE
@@ -25,26 +27,43 @@ IPADDR=$(hostname -i)
 fe/bin/start_fe.sh --daemon
 
 # Start Backend
+# This if you want to obtain the "tuned" result:
 echo "
 doris_scanner_thread_pool_thread_num=16
 tc_enable_aggressive_memory_decommit=true
 enable_new_scan_node=false
 mem_limit=100%
-" > be/conf/be_custom.conf
+" >be/conf/be_custom.conf
 
-# This if you want to obtain the "tuned" result:
-#echo "disable_storage_page_cache = false" >> be/conf/be.conf
-#echo "storage_page_cache_limit = 4G" >> be/conf/be.conf
-#echo "mem_limit=90%" >> be/conf/be.conf
+sudo sysctl -w vm.max_map_count=2000000
+be/bin/start_be.sh --daemon
 
-sudo sysctl -w vm.max_map_count=2000000 && "$DORIS_HOME"/output/be/bin/start_be.sh --daemon
-sleep 10
-cd ..
+# wait for Doris ready
+while true; do
+    fe_version=$(mysql -h127.0.0.1 -P9030 -uroot -e 'show frontends' | cut -f16 | sed -n '2,$p')
+    if [[ -n "${fe_version}" ]] && [[ "${fe_version}" != "NULL" ]]; then
+        echo "fe version: ${fe_version}"
+        break
+    else
+        echo 'wait for Doris fe started.'
+    fi
+    sleep 2
+done
+while true; do
+    be_version=$(mysql -h127.0.0.1 -P9030 -uroot -e 'show backends' | cut -f22 | sed -n '2,$p')
+    if [[ -n "${be_version}" ]]; then
+        echo "be version: ${be_version}"
+        break
+    else
+        echo 'wait for Doris be started.'
+    fi
+    sleep 2
+done
 
 # Setup cluster
 mysql -h 127.0.0.1 -P9030 -uroot -e "ALTER SYSTEM ADD BACKEND '${IPADDR}:9050' "
 mysql -h 127.0.0.1 -P9030 -uroot -e "CREATE DATABASE hits"
-mysql -h 127.0.0.1 -P9030 -uroot hits < create.sql
+mysql -h 127.0.0.1 -P9030 -uroot hits <create.sql
 
 # This if you want to obtain the "tuned" result:
 opt_session_variables="
@@ -54,7 +73,7 @@ enable_single_distinct_column_opt=true;
 enable_function_pushdown=true;
 enable_local_exchange=true;
 "
-for session_variable in ${opt_session_variables};do
+for session_variable in ${opt_session_variables}; do
     mysql -h 127.0.0.1 -P9030 -uroot -e "SET GLOBAL ${session_variable}"
 done
 mysql -h 127.0.0.1 -P9030 -uroot -e 'show variables' | grep 'exec_mem_limit\|parallel_fragment_exec_instance_num\|enable_single_distinct_column_opt\|enable_function_pushdown\|enable_local_exchange'
