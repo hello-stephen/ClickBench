fafb287573e109469670483eefad98ee3fc415ff
EC2 Default User <ec2-user@ip-172-31-88-123.ec2.internal>
2022-09-24 03:05:14 +0000

    fix

--- doris/benchmark.sh
+++ doris/benchmark.sh
@@ -10,6 +10,7 @@ if [[ -n "$1" ]]; then
     url="$1"
 else
     url='https://doris-build-1308700295.cos.ap-beijing.myqcloud.com/tmp/opt_perf-3d2a73c02-release-20220922221410.tar.gz'
+    url='https://doris-build-1308700295.cos.ap-beijing.myqcloud.com/tmp/opt_perf-72fdfc0e3-release-20220923220301.tar.gz'
 fi
 echo "Source bin from $url"
 
--- doris/get-compaction-info.sh
+++ doris/get-compaction-info.sh
@@ -6,7 +6,7 @@ set -o pipefail
 echo -e "\n\n\n\n
 #############################
 all rowsets, brief
-for i in \$(mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;' | sed -n '2,$p' | awk '{print $NF}');do curl "$i" 2>/dev/null | grep '\"\['; done
+for i in \$(mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;' | sed -n '2,\$p' | awk '{print \$NF}');do curl \$i 2>/dev/null | grep '\\\"\['; done
 #############################
 "
 for i in $(mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;' | sed -n '2,$p' | awk '{print $NF}'); do
@@ -14,15 +14,13 @@ for i in $(mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;' | sed 
     # curl "$i" 2>/dev/null
 done
 
-set +e
 echo -e "\n\n\n\n
 #############################
 all segments
 ls -alh $(cat doris_home)/be/storage/data/*/*/*
 #############################
 "
-ls -alh "$(cat doris_home)/be/storage/data/*/*/*"
-set -e
+ls -alh "$(cat doris_home)"/be/storage/data/*/*/*
 
 echo -e "\n\n\n\n
 #############################
@@ -35,7 +33,7 @@ mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;'
 echo -e "\n\n\n\n
 #############################
 all CompactionStatus
-for i in \$(mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;' | sed -n '2,$p' | awk '{print $NF}');do curl $i 2>/dev/null; done
+for i in \$(mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;' | sed -n '2,\$p' | awk '{print \$NF}');do curl \$i 2>/dev/null; done
 #############################
 "
 for i in $(mysql -h127.1 -P9030 -uroot -Dhits -e'show tablets from hits;' | sed -n '2,$p' | awk '{print $NF}'); do
