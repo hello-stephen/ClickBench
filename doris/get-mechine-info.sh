5476992ac3f86219277c1f8f2dbf2592711858d7
EC2 Default User <ec2-user@ip-172-31-82-81.ec2.internal>
2022-09-24 08:49:20 +0000

    add get mechine info

---
+++ doris/get-mechine-info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+echo -e "\n\n\n\n
+#############################
+cpu info
+lscpu
+#############################
+"
+lscpu
+
+echo -e "\n\n\n\n
+#############################
+DMI info
+sudo dmidecode
+#############################
+"
+sudo dmidecode
