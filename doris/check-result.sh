6f420ad9ae453662dda826852000c605b7da1227
EC2 Default User <ec2-user@ip-172-31-82-81.ec2.internal>
2022-09-30 00:56:15 +0000

    fix check-result.sh

--- doris/check-result.sh
+++ doris/check-result.sh
@@ -5,7 +5,6 @@ DORIS_HOME=$(cat doris_home)
 
 set +e
 ./kill-doris-cluster.sh
-set -e
 
 # Start Frontend
 "$DORIS_HOME"/fe/bin/start_fe.sh --daemon
@@ -16,22 +15,21 @@ sudo sysctl -w vm.max_map_count=2000000
 
 sleep 30
 
-if [[ ! -d doris-result ]];then mkdir doris-result;fi
+if [[ ! -d doris-result ]]; then mkdir doris-result; fi
 
 QUERY_NUM=1
-cat /home/ec2-user/ldy/ClickBench/doris/queries-sort.sql | while read query; do
-    echo $query
+while read -r query; do
+    echo "$query"
     mysql -h:: -P9030 -uroot -Dhits -e"$query" >doris-result/doris-q${QUERY_NUM}.result
     QUERY_NUM=$((QUERY_NUM + 1))
-done
+done <queries-sort.sql
 
-cd /home/ec2-user/ldy/ClickBench/clickhouse
-set +e
+cd ../clickhouse
 for i in {1..43}; do
-    echo 
-    echo query$i
-    sed -n '2,$p' ../doris/doris-result/doris-q$i.result >../doris/doris-result/doris-q$i.result2
-    diff ck-q$i.result ../doris/doris-result/doris-q$i.result2
+    echo
+    echo "query$i"
+    sed -n '2,$p' "../doris/doris-result/doris-q$i.result" >"../doris/doris-result/doris-q$i.result2"
+    # echo "diff -w \"ck-q$i.result\" \"../doris/doris-result/doris-q$i.result2\""
+    diff -w "ck-q$i.result" "../doris/doris-result/doris-q$i.result2"
     # read a
 done
-set -e
